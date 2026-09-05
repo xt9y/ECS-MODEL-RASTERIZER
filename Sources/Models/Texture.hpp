69690c1b17ed00f404915295751de087078d777b
xt9y <felix@xt9y.de>
2026-09-05 20:14:41 +0200

    flatten texture namespace

--- Sources/Models/Texture.hpp
+++ Sources/Models/Texture.hpp
@@ -6,7 +6,7 @@
 #include <cstdint>
 #include <string>
 
-namespace RW::Models {
+namespace Models {
 
 using TextureHandle = std::uint32_t;
 constexpr TextureHandle INVALID_TEXTURE = UINT32_MAX;
@@ -20,6 +20,6 @@ TextureHandle loadTexture(const std::string& path, std::string *error = nullptr)
 const TextureAsset *texture(TextureHandle handle);
 void clearTextureCache();
 
-} // namespace RW::Models
+} // namespace Models
 
 #endif
