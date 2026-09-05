2775ef59cc0b59e376be4eb77e495a24f2010b59
xt9y <felix@xt9y.de>
2026-09-06 00:20:23 +0200

    build: link shared lwcgl runtime

--- build.c
+++ build.c
@@ -5,22 +5,19 @@ static void configurePlatform(C_Target *target)
 #ifdef __APPLE__
     c_define(target, "GL_SILENCE_DEPRECATION");
     c_include(target, "/opt/homebrew/include");
-    c_include(target, "/usr/local/include");
     c_link_flag(target, "-L/opt/homebrew/lib");
-    c_link_flag(target, "-L/usr/local/lib");
-    c_link_system(target, "glfw");
     c_framework(target, "OpenGL");
     c_framework(target, "Cocoa");
     c_framework(target, "IOKit");
     c_framework(target, "CoreVideo");
 #else
-    c_link_system(target, "glfw");
     c_link_system(target, "GL");
     c_link_system(target, "GLU");
     c_link_system(target, "m");
     c_link_system(target, "dl");
     c_link_system(target, "pthread");
 #endif
+    c_link_system(target, "glfw");
 }
 
 static void configureLibrary(C_Target *target)
@@ -28,9 +25,18 @@ static void configureLibrary(C_Target *target)
     c_warnings_strict(target);
     c_include(target, ".");
     c_include(target, "Sources");
-    c_include(target, "/usr/local/include");
-    c_link_system(target, "lwcgl");
+    c_include(target, "/usr/local/include/lwcgl-2.9.3");
+
     configurePlatform(target);
+
+    c_link_flag(target, "-L/usr/local/lib");
+    c_link_flag(target, "-llwcgl-2.9.3");
+    c_link_flag(target, "-Wl,-rpath,/usr/local/lib");
+#ifdef __APPLE__
+    c_link_flag(target, "-Wl,-install_name,@rpath/libecs-model-rasterizer.dylib");
+#else
+    c_link_flag(target, "-Wl,-soname,libecs-model-rasterizer.so");
+#endif
 }
 
 void build(C_Build *b)
