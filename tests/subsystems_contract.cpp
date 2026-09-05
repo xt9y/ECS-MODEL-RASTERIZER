0b792755c84e27cd71154a7e6dfe7dadf73a71e1
xt9y <felix@xt9y.de>
2026-09-05 20:23:01 +0200

    update subsystem independence contract

--- tests/subsystems_contract.cpp
+++ tests/subsystems_contract.cpp
@@ -6,19 +6,19 @@
 #include <type_traits>
 
 static_assert(std::is_default_constructible_v<Ecs::World>);
-static_assert(std::is_default_constructible_v<Models::SpawnOptions>);
+static_assert(std::is_default_constructible_v<Models::ModelPart>);
 static_assert(std::is_default_constructible_v<Renderer::Rasterizer>);
 static_assert(std::is_default_constructible_v<Camera::Controller>);
 
 int main()
 {
     Ecs::World world;
+    Models::ModelPart part;
     Renderer::Rasterizer renderer;
     Camera::Controller camera;
-    Models::SpawnOptions options;
     (void)world;
+    (void)part;
     (void)renderer;
     (void)camera;
-    (void)options;
     return 0;
 }
