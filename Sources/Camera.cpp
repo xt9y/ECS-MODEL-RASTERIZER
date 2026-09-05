7e656151ab6c63c096d9ce9ffb78d25e42baa726
xt9y <felix@xt9y.de>
2026-09-05 22:31:38 +0200

    switch W and S movement

--- Sources/Camera.cpp
+++ Sources/Camera.cpp
@@ -48,8 +48,8 @@ void Controller::update(Ecs::World& world, float delta_seconds)
         transform->position.z += direction.z * scale;
     };
 
-    if (Keyboard.isKeyDown(Keyboard.KEY_W)) move(forward, speed);
-    if (Keyboard.isKeyDown(Keyboard.KEY_S)) move(forward, -speed);
+    if (Keyboard.isKeyDown(Keyboard.KEY_W)) move(forward, -speed);
+    if (Keyboard.isKeyDown(Keyboard.KEY_S)) move(forward, speed);
     if (Keyboard.isKeyDown(Keyboard.KEY_D)) move(right, -speed);
     if (Keyboard.isKeyDown(Keyboard.KEY_A)) move(right, speed);
 
