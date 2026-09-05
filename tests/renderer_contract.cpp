a8bdb8e4f25e1b670b92d38b5d05da835011f72f
xt9y <felix@xt9y.de>
2026-09-05 21:14:10 +0200

    test pre-GI lighting defaults

--- tests/renderer_contract.cpp
+++ tests/renderer_contract.cpp
@@ -2,6 +2,9 @@
 #include <type_traits>
 
 static_assert(std::is_default_constructible_v<Renderer::Rasterizer>);
+static_assert(Renderer::LightingDefaults::scene_ambient >= 0.5f);
+static_assert(Renderer::LightingDefaults::scene_ambient < 1.0f);
+static_assert(Renderer::LightingDefaults::direct_diffuse > 0.0f);
 
 int main()
 {
