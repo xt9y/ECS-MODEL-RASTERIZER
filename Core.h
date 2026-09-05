3f1a8935bb4bbb48cdc1822d582853cb68141843
xt9y <felix@xt9y.de>
2026-09-05 20:17:13 +0200

    remove Core umbrella header

--- Core.h
+++
@@ -1,9 +0,0 @@
-#ifndef RW_ENGINE_CORE_H
-#define RW_ENGINE_CORE_H
-
-#include "Sources/Engine.hpp"
-#include "Sources/Ecs/Ecs.hpp"
-#include "Sources/Models/Models.hpp"
-#include "Sources/Renderer/Render.hpp"
-
-#endif
