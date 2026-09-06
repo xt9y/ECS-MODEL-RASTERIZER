9c72166b5817979f089560d6af79d1eb58ac042b
xt9y <felix@xt9y.de>
2026-09-06 21:58:38 +0200

    models: split core types into subdirectory

---
+++ Sources/Models/Core/Types.hpp
@@ -0,0 +1,19 @@
+#ifndef RW_ENGINE_MODELS_CORE_TYPES_HPP
+#define RW_ENGINE_MODELS_CORE_TYPES_HPP
+
+namespace Models {
+
+struct Vec2 {
+    float x = 0.0f;
+    float y = 0.0f;
+};
+
+struct Vec3 {
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
+};
+
+} // namespace Models
+
+#endif
