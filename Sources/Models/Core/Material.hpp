869f9e8e23c6717fd26d72f671983a44a8d821dc
xt9y <felix@xt9y.de>
2026-09-06 21:59:27 +0200

    models: move material interface into Core

---
+++ Sources/Models/Core/Material.hpp
@@ -0,0 +1,31 @@
+#ifndef RW_ENGINE_MODELS_CORE_MATERIAL_HPP
+#define RW_ENGINE_MODELS_CORE_MATERIAL_HPP
+
+#include "Models/Core/Texture.hpp"
+#include "Models/Core/Types.hpp"
+
+#include <string>
+#include <unordered_map>
+
+namespace Models {
+
+struct MaterialData {
+    std::string name;
+    Vec3 color {1.0f, 1.0f, 1.0f};
+    float opacity = 1.0f;
+    std::string texture_path;
+    std::string opacity_texture_path;
+    TextureHandle diffuse_texture = INVALID_TEXTURE;
+};
+
+using MaterialMap = std::unordered_map<std::string, MaterialData>;
+
+bool loadMaterialLibrary(
+    const std::string& path,
+    MaterialMap *materials,
+    std::string *error = nullptr
+);
+
+} // namespace Models
+
+#endif
