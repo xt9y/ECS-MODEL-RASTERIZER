6e2e0813683346022cf8b4844e8eaa2cd6d9eaf6
xt9y <felix@xt9y.de>
2026-09-06 21:57:33 +0200

    ecs: move camera component into camera module

--- Sources/Camera.hpp
+++ Sources/Camera.hpp
@@ -2,11 +2,19 @@
 #define RW_ENGINE_CAMERA_HPP
 
 #include "Ecs/Ecs.hpp"
+#include "Renderer/Components.hpp"
 
 namespace Camera {
 
-Ecs::Vec3 flightDirection(float yaw_degrees, float pitch_degrees);
-Ecs::Vec3 strafeDirection(float yaw_degrees);
+struct CameraComponent {
+    float fov_degrees = 60.0f;
+    float near_plane = 0.1f;
+    bool active = true;
+};
+
+Ecs::Entity activeCamera(const Ecs::World& world);
+Renderer::Vec3 flightDirection(float yaw_degrees, float pitch_degrees);
+Renderer::Vec3 strafeDirection(float yaw_degrees);
 
 class Controller {
 public:
