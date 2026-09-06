8ca406f3e6a4b509642ae9d8273cf464b5722ea8
xt9y <felix@xt9y.de>
2026-09-06 22:00:09 +0200

    models: move OBJ interface into Formats

---
+++ Sources/Models/Formats/Obj.hpp
@@ -0,0 +1,24 @@
+#ifndef RW_ENGINE_MODELS_FORMATS_OBJ_HPP
+#define RW_ENGINE_MODELS_FORMATS_OBJ_HPP
+
+#include "Models/Models.hpp"
+
+#include <string>
+#include <vector>
+
+namespace Models::Obj {
+
+struct Part {
+    MeshData mesh;
+    MaterialData material;
+};
+
+struct Document {
+    std::vector<Part> parts;
+};
+
+bool load(const std::string& path, Document *document, std::string *error = nullptr);
+
+} // namespace Models::Obj
+
+#endif
