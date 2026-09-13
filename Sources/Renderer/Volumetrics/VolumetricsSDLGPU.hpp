c5f2413c2c23210add1e3d7eb658d32b3a033950
xt9y <felix@xt9y.de>
2026-09-13 11:45:30 +0200

    add sdl gpu volumetric interface

---
+++ Sources/Renderer/Volumetrics/VolumetricsSDLGPU.hpp
@@ -0,0 +1,19 @@
+#ifndef HORSE_RENDERER_VOLUMETRICS_VOLUMETRICS_SDLGPU_HPP
+#define HORSE_RENDERER_VOLUMETRICS_VOLUMETRICS_SDLGPU_HPP
+
+#include "Ecs/Ecs.hpp"
+#include "Renderer/Volumetrics/Volumetrics.hpp"
+
+namespace Renderer::Internal {
+struct FrameOutput;
+
+bool renderVolumetricsSDLGPU(
+    const Ecs::World& world,
+    FrameOutput& output,
+    const Volumetrics::Settings& settings
+);
+void shutdownVolumetricsSDLGPU();
+
+} // namespace Renderer::Internal
+
+#endif
